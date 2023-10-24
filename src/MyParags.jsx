0e224f53be6b63ff6bb499a3a192c444a9523b52
Ashimov ilya <anisimov_ilya_work@mail.ru>
2023-10-24 19:55:31 +0500

    add first parag

---
+++ src/MyParags.jsx
@@ -0,0 +1,7 @@
+export const MyParags = () => {
+    return (
+        <div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, laboriosam!</p>
+        </div>
+    ) 
+}
